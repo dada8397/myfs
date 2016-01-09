2d49687a7d7caf9465091947c50a4f09a3eba517
DADA <vincent8397@gmail.com>
2016-01-09 16:39:22 +0800

    Prototype initialize

--- myfs.h
+++ myfs.h
@@ -6,4 +6,14 @@
 #include <errno.h>
 #include <libgen.h>
 
+int myfs_create(const char *filesystemname, int max_size);
+int myfs_destroy(const char * filesystemname);
+
+int myfs_file_open(const char *filename);
+int myfs_file_close(int fd);
+int myfs_file_create(const char * filename);
+int myfs_file_delete(const char * filename);
+int myfs_file_read(int fd, char *buf, int count);
+int myfs_file_write(int fd, char *buf, int count);
+
 #endif
