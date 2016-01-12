ced37338a522ac3f2ef72768f9baad8fce6d392d
DADA <vincent8397@gmail.com>
2016-01-12 23:07:15 +0800

    Create filesystem

--- myfs.c
+++ myfs.c
@@ -5,7 +5,40 @@
 #include <string.h>
 
 
-/**********PARAMETERS**********/
+/* Parameters */
 FILE *fptr = NULL;
 
-/**********FUNCTIONS***********/
+/* Function Prototypes */
+
+int myfs_create(const char *filesystem_name, int max_size){
+    myfs_superblock_t superblock = {0};
+    myfs_inode_t inode[max_size / MAX_BLOCK_SIZE] = {0};
+
+    superblock.max_size = max_size;
+
+    superblock.inode_total = max_size / MAX_BLOCK_SIZE;
+    superblock.inode_bitmap_count = superblock.inode_total / (sizeof(uint) * 8);
+    superblock.block_total = max_size / MAX_BLOCK_SIZE;
+    superblock.block_bitmap_count = superblock.block_total / (sizeof(uint) * 8);
+
+    superblock.inode_bitmap_offset = sizeof(myfs_superblock_t);
+    superblock.block_bitmap_offset = superblock.inode_bitmap_offset
+                                   + superblock.inode_bitmap_count;
+    superblock.inode_offset = superblock.block_bitmap_offset
+                            + superblock.block_bitmap_count;
+    superblock.block_offset = superblock.inode_offset + superblock.inode_total;
+
+    uint inode_bitmap[superblock.inode_bitmap_count] = {0};
+    uint block_bitmap[superblock.block_bitmap_count] = {0};
+
+    FILE *new_fs_file = fopen(filesystem_name, "wb");
+    if(new_fs_file){
+        fwrite(&superblock, sizeof(myfs_superblock_t), 1, new_fs_file);
+        fwrite(&inode_bitmap, sizeof(uint), superblock.inode_bitmap_count, new_fs_file);
+        fwrite(&block_bitmap, sizeof(uint), superblock.block_bitmap_count, new_fs_file);
+        fwrite(&inode, sizeof(myfs_inode_t), max_size / MAX_BLOCK_SIZE, new_fs_file);
+        return SUCCESS;
+    }
+
+    return FAILURE;
+}
--- myfs.h
+++ myfs.h
@@ -10,19 +10,82 @@
 #include <errno.h>
 #include <libgen.h>
 
+/** Constants and Types **/
+typedef unsigned int            uint;
+typedef int                     BOOL;
+
+#define TRUE                    (1)
+#define FALSE                   (0)
+
+#define BUFFER_SIZE             (8192)
+#define ENTRY_EOF               (0x1b)
+
+// Superblock Constants
+#define MAX_FILE_NAME           (255)
+#define MAX_FILE_PATH           (4096)
+#define MAX_FILE_AMOUNT         (1024)
+
+// Inode Constants
+#define INODE_BLOCK_N           (15)
+
+// Block Constants
+#define MAX_BLOCK_SIZE          (4096)
+
+#define FAILURE                 (-1)
+#define SUCCESS                 (0)
+
 /* Type : File or Directory */
 typedef enum{
-    TYPE_DIRECTORY,
-    TYPE_FILE
+    TYPE_DIRECTORY  = 0x0001,
+    TYPE_FILE       = 0x0002
 } file_attribute_t;
 
+/* Superblock Structure */
+typedef struct myfs_superblock {
+    uint max_size;
+    uint unused_size;
 
+    uint inode_bitmap_offset;
+    uint inode_bitmap_count;
+    uint inode_offset;
+    uint inode_total;
+    uint inode_unused;
 
+    uint block_bitmap_offset;
+    uint block_bitmap_count;
+    uint block_offset;
+    uint block_total;
+    uint block_unused;
+} myfs_superblock_t;
+
+/* Inode Structure */
+typedef struct myfs_inode {
+    uint index;
+    uint mode;
+    uint next;
+    union {
+        uint child;
+        uint size;
+    };
+    uint block_count;
+    uint block[INODE_BLOCK_N];
+    char name[MAX_FILE_NAME];
+} myfs_inode_t;
+
+/* Block Structure */
+typedef struct myfs_block {
+
+} myfs_block_t;
+
+/* File System Prototypes */
 int myfs_create(const char *filesystem_name, int max_size);
 int myfs_destroy(const char *filesystem_name);
+
+/* File System Prototypes of Extra Functions */
 int myfs_open(const char *filesystem_name);
 int myfs_close(void);
 
+/* File Prototypes */
 int myfs_file_open(const char *filename);
 int myfs_file_close(int fd);
 int myfs_file_create(const char *filename);
@@ -30,6 +93,7 @@ int myfs_file_delete(const char *filename);
 int myfs_file_read(int fd, char *buffer, int count);
 int myfs_file_write(int fd, char *buffer, int count);
 
+/* File Prototypes of Extra Functions*/
 int myfs_mkdir(const char *filename);
 int myfs_rmdir(const char *filename);
 
